60d298a429a455fdef2e28d347b9e3eb58f6e2ee
saravanan <s.k.saravanan94@gmail.com>
2023-01-09 18:26:45 +0530

    Id change on button click

--- src/Component/DataFetchingOne.js
+++ src/Component/DataFetchingOne.js
@@ -4,19 +4,21 @@ import React, { useEffect, useState } from 'react'
 function DataFetchingOne(){
     const[post, setPost] = useState({})
     const[id, setId] = useState(1)
+    const[idButton, setIdButton] = useState(1)
 
     useEffect(() => {
         console.log("updated")
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idButton}`).then((res) => {
             setPost(res.data)
         }).catch((err) => {
             console.log(err);
         })
-    },[id])
+    },[idButton])
 
     return (
         <div>
             <input type='text' value={id} onChange={(e) => setId(e.target.value)}/>
+            <button onClick={() => setIdButton(id)}>Get Result</button>
             <div>{post.title}</div>
         </div>
     )
