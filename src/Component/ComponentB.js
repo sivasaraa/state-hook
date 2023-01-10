05cd9c95a3cbe452a1b72733d0344546c62b84c0
saravanan <s.k.saravanan94@gmail.com>
2023-01-10 20:02:08 +0530

    simplified using useContext

--- src/Component/ComponentB.js
+++ src/Component/ComponentB.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext, useReducer } from 'react'
 import ComponentC from './ComponentC'
+import { userContext, channelContext } from './ComponentA'
 
 function ComponentB(){
+    const user = useContext(userContext)
+    const channel = useContext(channelContext)
+    
     return (
         <div>
             <ComponentC />
+            <div>{user} - {channel}</div>
         </div>
     )
 }
